97fed1c5b4af75cc8833fca062ae6ada6a612011
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-02-26 23:26:44 -0800

    React Apollo GraphQL Server reference guide

---
+++ Examples/React-Apollo-GraphQL/Introduction/introduction.js
@@ -0,0 +1,29 @@
+/**
+ * In the previous tutorials, you learned about major concepts and benefits of GraphQL. Now is 
+ * the time to get your hands dirty and start out with an actual project!
+ * 
+ * You’re going to build a simple clone of Hackernews. Here’s a list of the features the app 
+ * will have:
+ *  - Display a list of links
+ *  - Search the list of links
+ *  - Users can authenticate
+ *  - Authenticated users can create new links
+ *  - Authenticated users can upvote links (one vote per link and user)
+ *  - Realtime updates when other users upvote a link or create a new one
+ * 
+ * In this track, you’ll use the following technologies for building the app:
+ *  Frontend:
+ *      React: Frontend framework for building user interfaces
+ *      Apollo Client 2.0: Production-ready, caching GraphQL client
+ *  Backend:
+ *      graphql-yoga: Fully-featured GraphQL Server with focus on easy setup, performance & great 
+ *          developer experience
+ *      Prisma: Open-source GraphQL API layer that turns your database into a GraphQL API
+ * 
+ * You’ll create the React project with create-react-app, a popular command-line tool that gives 
+ * you a blank project with all required build configuration already setup.
+ * 
+ * -----------------------
+ * Why a GraphQL Client ?
+ * -----------------------
+ */
