830b04eec1d1170f82b3f667dd1b3931f450711d
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-03-06 19:25:40 -0800

    GraphQL end point options

--- Apollo Server/Build GraphQL Server using Node/graphql-end-point-options.js
+++ Apollo Server/Build GraphQL Server using Node/graphql-end-point-options.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { graphqlExpress } from "apollo-server-express";
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const app = express();
 
 /**
